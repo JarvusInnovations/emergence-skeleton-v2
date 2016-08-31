9fba0c222f22615dac9bc4046d6b0f570a5a3f6f
Henry Paradiz <henry@jarv.us>
2016-08-30 22:02:01 -0400

    Emergence Developer Tools
    -Created a /.emr end point for Emergence Developer Tools
    -Moved emergence site-tools into Emergence Developer Tools and cleaned up templates
    -Created an isolated login template for Emergence Developer Tools so as not to interfere with the base site\'s designs.
    -Massive improvements to the git tool

--- php-classes/Emergence/Git/RequestHandler.php
+++ php-classes/Emergence/Git/RequestHandler.php
@@ -1,9 +1,7 @@
-<?php
-
+<?php	
 namespace Emergence\Git;
 
-use Site;
-use Exception;
+use Site,Exception,Git,Benchmark;
 
 class RequestHandler extends \RequestHandler
 {
@@ -17,19 +15,161 @@ class RequestHandler extends \RequestHandler
 		if (empty($_REQUEST['layer'])) {
 		    throw new Exception('Parameter "layer" required');
 		}
-
+		
 		$layerId = $_REQUEST['layer'];
-
+		
 		if(!$layer = Layer::getById($layerId)) {
 			throw new Exception('Requested layer "' . $layerId . '" is not defined');
 		}
-
+		
 		return $layer;
 	}
 
     public static function handleRequest()
     {
+        $GLOBALS['Session']->requireAccountLevel('Developer');
+        
+        switch($action ? $action : $action = static::shiftPath())
+    	{
+            case 'commit':
+                return static::handleCommitRequest();
+            case 'export':
+                return static::handleExportRequest();
+            case 'import':
+                return static::handleImportRequest();
+            case 'init':
+                return static::handleInitRequest();
+            case 'pull':
+                return static::handlePullRequest();
+            case 'push':
+                return static::handlePushRequest();
+            case 'status':
+                return static::handleStatusRequest();
+    	}
+        
         return static::throwNotFoundError();
+    }
+    
+    public static function handleCommitRequest()
+    {
+        // get repo
+        if (empty($_REQUEST['repo'])) {
+            die('Parameter "repo" required');
+        }
+        
+        $repoName = $_REQUEST['repo'];
+        
+        if (!array_key_exists($repoName, Git::$repositories)) {
+            die("Repo '$repoName' is not defined in Git::\$repositories");
+        }
+        
+        $repoCfg = Git::$repositories[$repoName];
+        
+        $exportOptions = array(
+            'localOnly' => false
+        );
+        
+        if (!empty($repoCfg['localOnly'])) {
+            $exportOptions['localOnly'] = true;
+        }
+        
+        // get message
+        if (empty($_POST['message'])) {
+            die(
+                '<form method="POST">'
+                    .'<label>Commit message: <input type="text" name="message" size="50"></label>'
+                    .'<input type="submit" value="Commit">'
+                .'</form>'
+            );
+        }
+        
+        
+        // start the process
+        set_time_limit(0);
+        Benchmark::startLive();
+        Benchmark::mark("configured request: repoName=$repoName");
+        
+        
+        // get paths
+        $repoPath = "$_SERVER[SITE_ROOT]/site-data/git/$repoName";
+        $keyPath = "$repoPath.key";
+        $gitWrapperPath = "$repoPath.git.sh";
+        putenv("GIT_SSH=$gitWrapperPath");
+        
+        
+        // check if there is an existing repo
+        if (!is_dir("$repoPath/.git")) {
+            die("$repoPath does not contain .git");
+        }
+        
+        
+        // get repo
+        chdir($repoPath);
+        $repo = new PHPGit_Repository($repoPath, !empty($_REQUEST['debug']));
+        Benchmark::mark("loaded git repo in $repoPath");
+        
+        
+        // verify repo state
+        if ($repo->getCurrentBranch() != $repoCfg['workingBranch']) {
+            die("Current branch in $repoPath is not $repoCfg[workingBranch]; aborting.");
+        }
+        Benchmark::mark("verified working branch");
+        
+        // sync trees
+        foreach ($repoCfg['trees'] AS $srcPath => $treeOptions) {
+            if (is_string($treeOptions)) {
+                $treeOptions = array(
+                    'path' => $treeOptions
+                );
+            }
+        
+            $treeOptions = array_merge($exportOptions, $treeOptions);
+        
+            if (!is_string($srcPath)) {
+                $srcPath = $treeOptions['path'];
+            } elseif (!$treeOptions['path']) {
+                $treeOptions['path'] = $srcPath;
+            }
+        
+            $srcFileNode = Site::resolvePath($srcPath);
+        
+            if (is_a($srcFileNode, 'SiteFile')) {
+                $destDir = dirname($treeOptions['path']);
+        
+                if ($destDir && !is_dir($destDir)) {
+                    mkdir($destDir, 0777, true);
+                }
+        
+                copy($srcFileNode->RealPath, $treeOptions['path']);
+                Benchmark::mark("exported file $srcPath to $treeOptions[path]");
+            } else {
+                $exportResult = Emergence_FS::exportTree($srcPath, $treeOptions['path'], $treeOptions);
+                Benchmark::mark("exported directory $srcPath to $treeOptions[path]: ".http_build_query($exportResult));
+            }
+        }
+        
+        
+        if (!empty($_REQUEST['syncOnly'])) {
+            exit();
+        }
+        
+        // set author
+        $repo->git(sprintf('git config user.name "%s"', $GLOBALS['Session']->Person->FullName));
+        $repo->git(sprintf('git config user.email "%s"', $GLOBALS['Session']->Person->Email));
+        
+        // commit changes
+        $repo->git('add --all');
+        
+        $repo->git(sprintf(
+            'commit -n -m "%s"'
+            ,addslashes($_POST['message'])
+        ));
+        Benchmark::mark("committed all changes");
+        
+        
+        // push changes
+        $repo->git("push origin $repoCfg[workingBranch]");
+        Benchmark::mark("pushed to $repoCfg[workingBranch]");
     }
 
 	public static function handleStatusRequest()
@@ -63,6 +203,160 @@ class RequestHandler extends \RequestHandler
 			'layers' => $layers
 		]);
 	}
+    
+    public static function handleImportRequest()
+    {
+        // get repo
+        if (empty($_REQUEST['repo'])) {
+            die('Parameter "repo" required');
+        }
+        
+        $repoName = $_REQUEST['repo'];
+        
+        if (!array_key_exists($repoName, Git::$repositories)) {
+            die("Repo '$repoName' is not defined in Git::\$repositories");
+        }
+        
+        $repoCfg = Git::$repositories[$repoName];
+        
+        
+        // start the process
+        set_time_limit(0);
+        Benchmark::startLive();
+        Benchmark::mark("configured request: repoName=$repoName");
+        
+        
+        // get paths
+        $repoPath = "$_SERVER[SITE_ROOT]/site-data/git/$repoName";
+        
+        // check if there is an existing repo
+        if (!is_dir("$repoPath/.git")) {
+            die("$repoPath does not contain .git");
+        }
+        
+        // get repo
+        chdir($repoPath);
+        
+        // sync trees
+        foreach ($repoCfg['trees'] AS $srcPath => $treeOptions) {
+            if (is_string($treeOptions)) {
+                $treeOptions = array(
+                    'path' => $treeOptions
+                );
+            }
+        
+            if (!is_string($srcPath)) {
+                $srcPath = $treeOptions['path'];
+            } elseif (!$treeOptions['path']) {
+                $treeOptions['path'] = $srcPath;
+            }
+        
+            if (is_string($treeOptions['exclude'])) {
+                $treeOptions['exclude'] = array($treeOptions['exclude']);
+            }
+        
+            $treeOptions['exclude'][] = '#(^|/)\\.git(/|$)#';
+            $treeOptions['dataPath'] = false;
+        
+            try {
+                if (is_file($treeOptions['path'])) {
+                    $sha1 = sha1_file($treeOptions['path']);
+                    $existingNode = Site::resolvePath($srcPath);
+        
+                    if (!$existingNode || $existingNode->SHA1 != $sha1) {
+                        $fileRecord = SiteFile::createFromPath($srcPath, null, $existingNode ? $existingNode->ID : null);
+                        SiteFile::saveRecordData($fileRecord, fopen($treeOptions['path'], 'r'), $sha1);
+                        Benchmark::mark("importing file $srcPath from $treeOptions[path]");
+                    } else {
+                        Benchmark::mark("skipped unchanged file $srcPath from $treeOptions[path]");
+                    }
+                } else {
+                    $exportResult = Emergence_FS::importTree($treeOptions['path'], $srcPath, $treeOptions);
+                    Benchmark::mark("importing directory $srcPath from $treeOptions[path]: ".http_build_query($exportResult));
+                }
+            } catch (Exception $e) {
+                Benchmark::mark("failed to import directory $srcPath from $treeOptions[path]: ".$e->getMessage());
+            }
+        }
+    }
+    
+    public static function handleExportRequest()
+    {
+
+        // get repo
+        if (empty($_REQUEST['repo'])) {
+            die('Parameter "repo" required');
+        }
+        
+        $repoName = $_REQUEST['repo'];
+        
+        if (!array_key_exists($repoName, Git::$repositories)) {
+            die("Repo '$repoName' is not defined in Git::\$repositories");
+        }
+        
+        $repoCfg = Git::$repositories[$repoName];
+        
+        $exportOptions = array(
+            'localOnly' => false
+        );
+        
+        if (!empty($repoCfg['localOnly'])) {
+            $exportOptions['localOnly'] = true;
+        }
+        
+        // start the process
+        set_time_limit(0);
+        Benchmark::startLive();
+        Benchmark::mark("configured request: repoName=$repoName");
+        
+        
+        // get paths
+        $repoPath = "$_SERVER[SITE_ROOT]/site-data/git/$repoName";
+        
+        // check if there is an existing repo
+        if (!is_dir("$repoPath/.git")) {
+            die("$repoPath does not contain .git");
+        }
+        
+        
+        // get repo
+        chdir($repoPath);
+        
+        // sync trees
+        foreach ($repoCfg['trees'] AS $srcPath => $treeOptions) {
+            if (is_string($treeOptions)) {
+                $treeOptions = array(
+                    'path' => $treeOptions
+                );
+            }
+        
+            $treeOptions = array_merge($exportOptions, $treeOptions, ['dataPath' => false]);
+        
+            if (!is_string($srcPath)) {
+                $srcPath = $treeOptions['path'];
+            } elseif (!$treeOptions['path']) {
+                $treeOptions['path'] = $srcPath;
+            }
+        
+            $srcFileNode = Site::resolvePath($srcPath);
+        
+            if (is_a($srcFileNode, 'SiteFile')) {
+                $destDir = dirname($treeOptions['path']);
+        
+                if ($destDir && !is_dir($destDir)) {
+                    mkdir($destDir, 0777, true);
+                }
+        
+                copy($srcFileNode->RealPath, $treeOptions['path']);
+                Benchmark::mark("exported file $srcPath to $treeOptions[path]");
+            } else {
+                $exportResult = Emergence_FS::exportTree($srcPath, $treeOptions['path'], $treeOptions);
+                Benchmark::mark("exported directory $srcPath to $treeOptions[path]: ".http_build_query($exportResult));
+            }
+        }
+        
+        Benchmark::mark("wrote all changes");
+    }
 
 	public static function handleInitRequest()
 	{
@@ -75,30 +369,14 @@ class RequestHandler extends \RequestHandler
 		}
 
 		if ($_SERVER['REQUEST_METHOD'] != 'POST') {
-            $keyTmpPath = tempnam(sys_get_temp_dir(), 'git-key');
-            unlink($keyTmpPath);
-            $keyStatus = exec($command = "ssh-keygen -q -t rsa -N '' -C '".Site::getConfig('primary_hostname')."' -f '$keyTmpPath'; echo $?");
-
-            if ($keyStatus != '0') {
-                throw new Exception("Failed to execute command: $command\nExit output: $keyStatus");
-            }
-
-            $publicKey = file_get_contents("$keyTmpPath.pub");
-            $privateKey = file_get_contents($keyTmpPath);
-
-            unlink("$keyTmpPath.pub");
-            unlink($keyTmpPath);
-
 			return static::respond('configureInit', [
-				'layer' => $layer,
-                'publicKey' => $publicKey,
-                'privateKey' => $privateKey
+				'layer' => $layer
 			]);
 		}
 
 		return static::respond('repositoryInitialized', [
 			'layer' => $layer,
-			'results' => $layer->initializeRepository($_POST['privateKey'], $_POST['publicKey'])
+			'results' => $layer->initializeRepository($_POST['privateKey'])
 		]);
 	}
 
